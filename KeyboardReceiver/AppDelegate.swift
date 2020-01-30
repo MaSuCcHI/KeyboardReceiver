7e75b737cf4a0dbd3aa3a6ffc84df619a0205882
Keisuke <39832596+MaSuCcHI@users.noreply.github.com>
2020-01-31 03:04:13 +0900

    Initial Commit

---
+++ KeyboardReceiver/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  KeyboardReceiver
+//
+//  Created by KEISUKE MASUDA on 2020/01/31.
+//  Copyright © 2020 KEISUKE MASUDA. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ KeyboardReceiver/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  KeyboardReceiver
+//
+//  Created by KEISUKE MASUDA on 2020/01/31.
+//  Copyright © 2020 KEISUKE MASUDA. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
